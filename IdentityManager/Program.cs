e2e9645aaccfe89acfd8256fef4a938da24adb8d
Anand-wase <anandvardhan.16pav@gmail.com>
2023-08-24 12:29:24 +0530

    Add Lockout On Failure

--- IdentityManager/Program.cs
+++ IdentityManager/Program.cs
@@ -1,13 +1,35 @@
 using IdentityManager.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Cqrs.Hosts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireLowercase = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+//builder.Services.Configure<IdentityOptions>(options =>
+//{
+//    // Default Lockout settings.
+//    options.Password.RequiredLength = 5;
+//    options.Password.RequireLowercase = true;
+//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(5);
+//    options.Lockout.MaxFailedAccessAttempts = 5;
+//    options.Lockout.AllowedForNewUsers = true;
+//});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
